14270517efc76daf71ff6925e271354ead4fdd4f
Brian McCarthy <brianmccarthy1@outlook.ie>
2024-01-28 23:57:03 +0000

    chore: update error message

--- SoccerManager/Validators/CreateValidator.cs
+++ SoccerManager/Validators/CreateValidator.cs
@@ -7,7 +7,7 @@ public class CreateValidator : AbstractValidator<Player>
     public CreateValidator()
     {
         RuleFor(p => p.Name).NotEmpty().WithMessage("Name is required");
-        RuleFor(p => p.Position).NotEmpty().IsInEnum().WithMessage("Position is required");
+        RuleFor(p => p.Position).NotEmpty().IsInEnum().WithMessage("Position is required and must be between 1 and 4 inclusive");
         RuleFor(p => p.SkillRating).InclusiveBetween(1, 100).WithMessage("Skill Rating must be between 1 and 100");
     }
 }
