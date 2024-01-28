79fba96475d96ee87bed1c0a7dac0c6506de6b4f
Brian McCarthy <brianmccarthy1@outlook.ie>
2024-01-28 16:45:11 +0000

    feat: add ability to run app

--- SoccerManager/Program.cs
+++ SoccerManager/Program.cs
@@ -1,20 +1,18 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SoccerManager.Controller;
 using SoccerManager.Models;
 using SoccerManager.Services;
 using SoccerManager.Validators;
 
 var builder = Host.CreateDefaultBuilder(args);
-builder.ConfigureServices((_, services) =>
+var host = builder.ConfigureServices((_, services) =>
 {
     services.AddSingleton<IPlayerService, PlayerService>();
     services.AddSingleton<AbstractValidator<Player>, CreateValidator>();
-});
-builder.ConfigureLogging(logger =>
-{
-    logger.AddConsole();
-});
+    services.AddSingleton<IPlayerController, PlayerController>();
+}).Build();
 
-builder.Build().Run();
+using var app = host.Services.CreateScope();
+app.ServiceProvider.GetRequiredService<IPlayerController>().Run();
